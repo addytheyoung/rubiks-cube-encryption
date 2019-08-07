b967f08ec1bee0f7d62f8cd9d40c5bc5a7eed077
Dwarkesh Patel <dwarkesh.sanjay.patel@gmail.com>
2019-08-06 20:50:30 -0500

    partial message to message method

--- src/components/Cube.js
+++ src/components/Cube.js
@@ -1,6 +1,20 @@
 import React, { Component, useState } from "react";
 
-const newCube = (size, message) => {};
+const newCube = (size, partialMessage) => {
+  const message = partialMessage => {
+    const cubeSize = size * size * 6;
+    if (cubeSize === partialMessage.length) {
+      return partialMessage;
+    }
+    // null character
+    partialMessage.push(0);
+    // add random characters for remaining length
+    for (let i = partialMessage.length; i < cubeSize; i++) {
+      partialMessage.push(Math.random() * 58 + 65);
+    }
+    return partialMessage;
+  };
+};
 
 const CubeGrid = Cube => {
   return <div>eorgnva</div>;
