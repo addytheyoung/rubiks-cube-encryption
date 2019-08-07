edba80f4b71a5d5618b6a1c1f3166e5187dd25ac
Dwarkesh Patel <dwarkesh.sanjay.patel@gmail.com>
2019-08-06 23:02:30 -0500

    newCube and CubeGrid progress

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import Cube from "./components/Cube";
 function App() {
   return (
     <div>
-      <Cube />
+      <Cube message={message} />
     </div>
   );
 }
+
+const message =
+  "It is a cliché, of course, to claim that a presidential election is the most important in living memory. But we arrived at that point in the 2016 campaign many months ago, when both sides declared their opponent unqualified for office. Unfortunately, this time the cliché is true, and one side is actually right. A choice this stark proves that there is something wrong with our political system. Hillary Clinton is a terribly flawed candidate for the presidency, and this has allowed millions of otherwise sane Americans to imagine that she is less fit for office than Donald Trump is.Much depends on a majority of the electorate seeing through this moral and political illusion in the weeks ahead.";
 
 export default App;
--- src/components/Cube.js
+++ src/components/Cube.js
@@ -1,7 +1,7 @@
 import React, { Component, useState } from "react";
 
-const newCube = (size, partialMessage) => {
-  const message = partialMessage => {
+const newCube = (partialMessage, size) => {
+  const message = () => {
     const cubeSize = size * size * 6;
     if (cubeSize === partialMessage.length) {
       return partialMessage;
@@ -10,25 +10,81 @@ const newCube = (size, partialMessage) => {
     partialMessage.push(0);
     // add random characters for remaining length
     for (let i = partialMessage.length; i < cubeSize; i++) {
-      partialMessage.push(Math.random() * 58 + 65);
+      partialMessage[i] = Math.random() * 58 + 65;
     }
     return partialMessage;
   };
+
+  function Sticker(character, color) {
+    // might change to raw later
+    this.character = String.fromCharCode(character);
+    this.color = color;
+  }
+
+  const cube = [];
+  let cubeIndex = 0;
+  for (let i = 0; i < size; i++) {
+    cube[i] = [];
+    for (let j = size; j < 2 * size; j++) {
+      cube[i][j] = new Sticker(message[cubeIndex], "white");
+      cubeIndex++;
+    }
+  }
+  for (let i = size; i < 2 * size; i++) {
+    cube[i] = [];
+    for (let j = 0; j < size; j++) {
+      cube[i][j] = new Sticker(message[cubeIndex], "orange");
+      cubeIndex++;
+    }
+    for (let j = size; j < 2 * size; j++) {
+      cube[i][j] = new Sticker(message[cubeIndex], "green");
+      cubeIndex++;
+    }
+    for (let j = 2 * size; j < 3 * size; j++) {
+      cube[i][j] = new Sticker(message[cubeIndex], "red");
+      cubeIndex++;
+    }
+    for (let j = 3 * size; j < 4 * size; j++) {
+      cube[i][j] = new Sticker(message[cubeIndex], "blue");
+      cubeIndex++;
+    }
+  }
+  for (let i = 2 * size; i < 3 * size; i++) {
+    cube[i] = [];
+    for (let j = size; j < 2 * size; j++) {
+      cube[i][j] = new Sticker(message[cubeIndex], "yellow");
+      cubeIndex++;
+    }
+  }
+  return cube;
 };
 
-const CubeGrid = Cube => {
-  return <div>eorgnva</div>;
+const CubeGrid = ({ cube, size }) => {
+  const tr = [];
+  for (let i = 0; i < 3 * size; i++) {
+    const td = [];
+    for (let j = 0; j < 4 * size; cube++) {
+      td.push(<td key={`${i},${j}`} color={cube[i][j].color} />);
+    }
+    tr.push(<tr key={r}>{td}</tr>);
+  }
+  return (
+    <table>
+      <tbody>{tr}</tbody>
+    </table>
+  );
 };
 
 // const Slider = ({ speed, onSpeedChange }) => {};
 
 export default function Cube(props) {
-  const { size, message } = props;
-  const [Cube, setCube] = useState(newCube(size, message));
+  const { message } = props;
+  const size = Math.ceil(Math.sqrt(message.length / 6.0));
+  const [Cube, setCube] = useState(newCube(message, size));
 
   return (
     <div>
-      <CubeGrid Cube={Cube} />
+      <CubeGrid cube={Cube} size={size} />
     </div>
   );
 }
