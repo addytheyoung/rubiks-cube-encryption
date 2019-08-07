1ac5d8c44a3851236da5ab5ff997db4fe91e3551
Dwarkesh Patel <dwarkesh.sanjay.patel@gmail.com>
2019-08-07 12:01:15 -0500

    newCube fixed

--- src/components/Cube.js
+++ src/components/Cube.js
@@ -1,7 +1,7 @@
-import React, { Component, useState } from "react";
+import React, { useState } from "react";
 
 const newCube = (partialMessage, size) => {
-  const message = () => {
+  const generateFullMessage = () => {
     const cubeSize = size * size * 6;
     if (cubeSize === partialMessage.length) {
       return partialMessage;
@@ -10,14 +10,14 @@ const newCube = (partialMessage, size) => {
     partialMessage.push(0);
     // add random characters for remaining length
     for (let i = partialMessage.length; i < cubeSize; i++) {
-      partialMessage[i] = Math.random() * 58 + 65;
+      partialMessage[i] = String.fromCharCode(Math.random() * 58 + 65);
     }
     return partialMessage;
   };
-
+  const message = generateFullMessage();
+  // constructor for grid elements
   function Sticker(character, color) {
-    // might change to raw later
-    this.character = String.fromCharCode(character);
+    this.character = character;
     this.color = color;
   }
 
@@ -25,9 +25,15 @@ const newCube = (partialMessage, size) => {
   let cubeIndex = 0;
   for (let i = 0; i < size; i++) {
     cube[i] = [];
+    for (let j = 0; j < size; j++) {
+      cube[i][j] = new Sticker("", "black");
+    }
     for (let j = size; j < 2 * size; j++) {
       cube[i][j] = new Sticker(message[cubeIndex], "white");
       cubeIndex++;
+    }
+    for (let j = 2 * size; j < 4 * size; j++) {
+      cube[i][j] = new Sticker("", "black");
     }
   }
   for (let i = size; i < 2 * size; i++) {
@@ -51,23 +57,31 @@ const newCube = (partialMessage, size) => {
   }
   for (let i = 2 * size; i < 3 * size; i++) {
     cube[i] = [];
+    for (let j = 0; j < size; j++) {
+      cube[i][j] = new Sticker("", "black");
+    }
     for (let j = size; j < 2 * size; j++) {
       cube[i][j] = new Sticker(message[cubeIndex], "yellow");
       cubeIndex++;
+    }
+    for (let j = 2 * size; j < 4 * size; j++) {
+      cube[i][j] = new Sticker("", "black");
     }
   }
   return cube;
 };
 
 const CubeGrid = ({ cube, size }) => {
+  console.log(cube);
   const tr = [];
   for (let i = 0; i < 3 * size; i++) {
     const td = [];
-    for (let j = 0; j < 4 * size; cube++) {
+    for (let j = 0; j < 4 * size; j++) {
       td.push(<td key={`${i},${j}`} color={cube[i][j].color} />);
     }
-    tr.push(<tr key={r}>{td}</tr>);
+    tr.push(<tr key={i}>{td}</tr>);
   }
+
   return (
     <table>
       <tbody>{tr}</tbody>
@@ -78,9 +92,11 @@ const CubeGrid = ({ cube, size }) => {
 // const Slider = ({ speed, onSpeedChange }) => {};
 
 export default function Cube(props) {
-  const { message } = props;
+  const message = props.message.split("");
   const size = Math.ceil(Math.sqrt(message.length / 6.0));
   const [Cube, setCube] = useState(newCube(message, size));
+
+  const twistCube = () => {};
 
   return (
     <div>
